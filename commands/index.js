de5050c2b52610bc392c794f9854950c02a384e7
CosJames <51408660+CosJames@users.noreply.github.com>
2021-02-26 01:18:59 +0800

    Minor wrong naming patch

--- commands/index.js
+++ commands/index.js
@@ -1,7 +1,7 @@
 let SongQueue = require('./SongQueue')
 let MemberPowers = require('./MemberPowers')
 let AdministratorPowers = require('./AdministratorPowers')
-
+console.log('Initializing files...')
 module.exports = {
     SongQueue: SongQueue,
     MemberPowers: MemberPowers,
